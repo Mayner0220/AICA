e8d63d32297fc83de1a577d8943fa84423c60e64
Shim Jung Hyun <shj030220@gmail.com>
2021-04-01 09:21:19 +0900

    [Feat] Initialization about AUTOTUNE, batch size, image size, epochs

--- setup.py
+++ setup.py
@@ -6,3 +6,8 @@ strategy = tf.distribute.get_strategy()
 
 print("Number of replicas: {0}".format(strategy.num_replicas_in_sync))
 print("TF version: {0}".format(tf.__version__))
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+BATCH_SIZE = 16 * strategy
+IMAGE_SIZE = [176, 208]
+EPOCHS = 100
