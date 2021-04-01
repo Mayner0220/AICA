438ddf9d121705fe0e2e6febb889c99f9cfe7cd7
Shim Jung Hyun <shj030220@gmail.com>
2021-04-01 09:03:51 +0900

    [Feat] Check number of replicas

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ import tensorflow as tf
 
 strategy = tf.distribute.get_strategy()
 
+print("Number of replicas: {0}".format(strategy.num_replicas_in_sync))
 print("TF version: {0}".format(tf.__version__))
