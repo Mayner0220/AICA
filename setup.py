a4584fa40bf3e9969d97564ec39ad384a1ee1a04
Shim Jung Hyun <shj030220@gmail.com>
2021-04-01 09:24:04 +0900

    [Style] Add comment

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ print("Number of replicas: {0}".format(strategy.num_replicas_in_sync))
 print("TF version: {0}".format(tf.__version__))
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+# Starting batch size: 16
+# Starting epoch: 100
+# IMAGE SIZE: 176 * 208
+# 이 값들은 차후적으로 튜닝을 진행하면서 변화할 예정
 BATCH_SIZE = 16 * strategy
 IMAGE_SIZE = [176, 208]
 EPOCHS = 100
