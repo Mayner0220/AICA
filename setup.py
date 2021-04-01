f9c267da05dc3bf6762f84ac4288af54d3e5aae1
Shim Jung Hyun <shj030220@gmail.com>
2021-04-01 11:33:21 +0900

    [Fix] Add num_replicas_in_sync

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 # Starting epoch: 100
 # IMAGE SIZE: 176 * 208
 # 이 값들은 차후적으로 튜닝을 진행하면서 변화할 예정
-BATCH_SIZE = 16 * strategy
+BATCH_SIZE = 16 * strategy.num_replicas_in_sync
 IMAGE_SIZE = [176, 208]
 EPOCHS = 100
